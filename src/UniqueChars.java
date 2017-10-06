91eaae9d3dd2b3bab8b172103bcdbcde8263a74f
kaghee <kass.agnes@gmail.com>
2017-10-06 12:57:23 +0200

    codes cleaned up and finalized

--- src/UniqueChars.java
+++ src/UniqueChars.java
@@ -5,7 +5,7 @@ public class UniqueChars {
         // Create a function called `uniqueCharacters` that takes a string as parameter
         // and returns a list with the unique letters of the given string
         // Create basic unit tests for it with at least 3 different test cases
-        System.out.println(uniqueCharacters(""));
+        System.out.println(uniqueCharacters("anagram"));
         // Should print out:
         // ["n", "g", "r", "m"]
     }
