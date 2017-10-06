aa124e44e40f23086b0cfe87b80d151381373d33
kaghee <kass.agnes@gmail.com>
2017-10-06 10:14:44 +0200

    FavAnimals adds animals to the file from arguments

--- src/FavouriteAnimals.java
+++ src/FavouriteAnimals.java
@@ -1,3 +1,8 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class FavouriteAnimals {
     public static void main(String[] args) {
@@ -10,5 +15,37 @@ public class FavouriteAnimals {
         // One animal should be stored only at once
         // Each animal should be written in separate lines
         // The program should only save animals, no need to print them
+
+        FavouriteAnimals app = new FavouriteAnimals();
+
+        if (args.length == 0) {
+            app.manual();
+        } else {
+            for (int i = 0; i < args.length; i++) {
+                app.addAnimal(args[i]);
+            }
+        }
     }
+
+    public FavouriteAnimals() {
+
+    }
+
+    public void manual() {
+        System.out.println("How to use:\njava FavouriteAnimals [animal] [animal]");
+    }
+
+    public void addAnimal(String animal) {
+        Path filepath = Paths.get("favourites.txt");
+        try {
+            List<String> animals = Files.readAllLines(filepath);
+            animals.add(animal);
+            Files.write(filepath, animals);
+
+        } catch (IOException e) {
+            System.out.println("Unable to read file");
+        }
+
+    }
+
 }
